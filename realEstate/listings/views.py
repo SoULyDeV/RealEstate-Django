0feb1b16d595d9490925d5a9ccf1f516795383fe
SoULyDeV <josserand21@hotmail.com>
2023-06-18 21:33:08 +0400

    First commit here

---
+++ realEstate/listings/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from .models import Listing
+# Create your views here.
+
+#CRUD create, retrieve, update, delete 'list'
+
+
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings.html', context)
+
+def listing_retrieve(request, pk):
+    listing = Listing.objects.get(id=pk)
+    context = {'listing': listing}
+    
+    return render(request, 'listing.html', context)
