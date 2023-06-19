1937d7955657bdfd5914b2f8252b16af4b15337d
SoULyDeV <josserand21@hotmail.com>
2023-06-19 19:16:34 +0400

    second commit

--- realEstate/listings/views.py
+++ realEstate/listings/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Listing
 # Create your views here.
+from .forms import ListingForm
 
 #CRUD create, retrieve, update, delete 'list'
 
@@ -19,3 +20,36 @@ def listing_retrieve(request, pk):
     context = {'listing': listing}
     
     return render(request, 'listing.html', context)
+
+def listing_create(request):
+    #bellow how to handling the posts values
+    form = ListingForm()
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+           form.save()
+           return redirect('/')
+            
+    context = {
+        'form': form
+    }
+    return render(request, 'listing_create.html', context)
+    
+def listing_uptdate(request):
+    
+    listing = Listing.objects.get(id=pk)
+    
+    #with 'instance' we pick only what we fetch , here listing
+    form = ListingForm(instance = listing)
+    
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance = listing)
+        if form.is_valid():
+           form.save()
+           return redirect('/')
+            
+    context = {
+        'form': form
+    }
+    return render(request, 'listing_update.html', context)
+    
--- realEstate/realEstate/urls.py
+++ realEstate/realEstate/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from listings.views import listing_list, listing_retrieve
+from listings.views import listing_list, listing_retrieve, listing_create, listing_uptdate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listing_list),
     path('listing/<pk>/', listing_retrieve),
+    path('listing/<pk>/edit/', listing_uptdate),
+    path('add_listing/', listing_create),
 ]
